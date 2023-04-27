a6550f8479332e7d9e120c798ad426d026732c7d
Zoe <zoews98@gmail.com>
2023-04-27 23:21:45 +0800

    setup commit

--- setup.py
+++ setup.py
@@ -67,11 +67,28 @@ class Setup(object):
         assert values.basis == Basis.LAGRANGE
 
         # Run inverse FFT to convert values from Lagrange basis to monomial basis
+        mono_values = values.ifft().values
         # Optional: Check values size does not exceed maximum power setup can handle
+        if len(mono_values) > len(self.powers_of_x):
+            raise Exception("not enough power of polynomial")
+        return ec_lincomb([(s, x) for s, x in zip(self.powers_of_x, mono_values)])
         # Compute linear combination of setup with values
-        return NotImplemented
+        #return NotImplemented
 
     # Generate the verification key for this program with the given setup
     def verification_key(self, pk: CommonPreprocessedInput) -> VerificationKey:
         # Create the appropriate VerificationKey object
-        return NotImplemented
+        return VerificationKey(
+            pk.group_order,
+            self.commit(pk.QM),
+            self.commit(pk.QL),
+            self.commit(pk.QR),
+            self.commit(pk.QO),
+            self.commit(pk.QC),
+            self.commit(pk.S1),
+            self.commit(pk.S2),
+            self.commit(pk.S3),
+            self.X2,
+            Scalar.root_of_unity(pk.group_order)
+        )
+        #return NotImplemented
